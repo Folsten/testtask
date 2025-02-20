a6efdff32622d05c44337b4c255a1bafc3ef0ad8
Polyakov Alexey <mega4642179@gmail.com>
2025-02-20 17:14:27 +0300

    refactor: remove general comments api request from app.vue

--- app.vue
+++ app.vue
@@ -12,10 +12,8 @@ const mainStore = useMainStore();
 const [users, posts, comments] = await Promise.all([
   $fetch("https://dummyjson.com/users?limit=0"),
   $fetch("https://dummyjson.com/posts?limit=0"),
-  $fetch("https://dummyjson.com/comments?limit=0"),
 ]);
 
 mainStore.users = users.users;
 mainStore.posts = posts.posts;
-mainStore.comments = comments.comments;
 </script>
